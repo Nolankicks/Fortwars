18cb89aed74b7898cfd8074a626846137652a6a3
Nolankicks <nolandimensions@gmail.com>
2024-10-26 17:57:55 -0400

    Allow placing of props with mouse. Increase ssao

--- code/Player/Weapons/Propgun.cs
+++ code/Player/Weapons/Propgun.cs
@@ -7,6 +7,7 @@ public sealed class Propgun : Item
 	public static bool FirstTime { get; set; } = true;
 	public Angles PropRotation { get; set; }
 	[Property] bool MustBeUp { get; set; } = false;
+	[Property] public bool UsingMouseInput { get; set; } = false;
 
 	protected override void OnStart()
 	{
@@ -46,6 +47,15 @@ public sealed class Propgun : Item
 			PopupHolder.AddPopup( attack1 );
 		} );
 
+		Invoke( 8, () =>
+		{
+			var attack2 = new Popup();
+			attack2.Title = $"Press {Input.GetButtonOrigin( "mouseprop" )?.ToUpper()} to toggle mouse input";
+			attack2.Time = 8;
+
+			PopupHolder.AddPopup( attack2 );
+		} );
+
 		FirstTime = false;
 	}
 
@@ -53,6 +63,14 @@ public sealed class Propgun : Item
 	{
 		if ( IsProxy )
 			return;
+
+		if ( Input.Pressed( "mouseprop" ) )
+		{
+			Mouse.Visible = !Mouse.Visible;
+			UsingMouseInput = !UsingMouseInput;
+
+			PropRotation = Rotation.Identity;
+		}
 
 		if ( Input.Pressed( "menu" ) )
 		{
@@ -70,159 +88,13 @@ public sealed class Propgun : Item
 
 		if ( Prop is not null )
 		{
-			var player = PlayerController.Local;
-
-			if ( !player.IsValid() )
-				return;
-
-			Vector3 ObjectPos = player.Eye.WorldPosition + player.Eye.WorldRotation.Forward * 100.0f;
-
-			var tr = Scene.Trace
-				.Box( Prop.Bounds.Rotate( PropRotation ) * 0.75f, player.Eye.WorldPosition, player.Eye.WorldPosition + player.Eye.WorldRotation.Forward * 200.0f )
-				.IgnoreGameObjectHierarchy( GameObject.Root )
-				.Run();
-
-			if ( tr.Hit )
+			if ( UsingMouseInput )
 			{
-				ObjectPos = tr.EndPosition;
+				PlacePropMouse();
 			}
-
-
-
-			ObjectPos = ObjectPos.SnapToGrid( 16, true, true, !(tr.Hit && tr.Normal == Vector3.Up) );
-
-			bool CanPlace = tr.Hit && tr.Distance > 32.0f && ObjectPos.Distance( GameObject.Root.WorldPosition ) > 32.0f && !tr.GameObject.Tags.Has( FW.Tags.NoBuild );
-
-			var gizmo = Gizmo.Draw.Model( Prop.ResourcePath );
-			gizmo.ColorTint = Color.White.WithAlpha( 0.5f );
-			gizmo.Rotation = PropRotation.SnapToGrid( 15 );
-			gizmo.Position = ObjectPos;
-
-
-			if ( !CanPlace )
+			else
 			{
-				gizmo.ColorTint = Color.Red.WithAlpha( 0.5f );
-			}
-
-			player.CanMoveHead = !Input.Down( "attack2" );
-
-			if ( tr.Hit && Input.Pressed( "destroy" ) && (tr.GameObject?.Root?.Components.TryGet<FortwarsProp>( out var prop, FindMode.EverythingInSelfAndDescendants ) ?? false) )
-			{
-				if ( prop.Invincible )
-					return;
-
-				tr.GameObject?.Root?.Destroy();
-			}
-
-			if ( Input.Down( "attack2" ) )
-			{
-				var rot = Input.AnalogLook.WithRoll( 0 );
-
-				PropRotation += new Angles( -rot.pitch, -rot.yaw, 0 );
-			}
-
-			if ( Input.Pressed( "reload" ) )
-			{
-				PropRotation = Rotation.Identity;
-			}
-
-
-			if ( CanPlace && Input.Pressed( "attack1" ) )
-			{
-				var sound = Sound.Play( "sounds/toolgun/toolgun-fire.sound", WorldPosition );
-				sound.Volume = 0.5f;
-
-				var currentTeam = player.TeamComponent?.Team;
-
-				var gs = GameSystem.Instance;
-
-				if ( !gs.IsValid() )
-					return;
-
-				var hud = Scene.GetAll<HUD>()?.FirstOrDefault();
-
-				switch ( currentTeam )
-				{
-					case Team.Red:
-						if ( gs.RedProps.Count() >= gs.MaxProps )
-						{
-							hud?.FlashPropsFailed();
-							return;
-						}
-						break;
-					case Team.Blue:
-						if ( gs.BlueProps.Count() >= gs.MaxProps )
-						{
-							hud?.FlashPropsFailed();
-							return;
-						}
-						break;
-					case Team.Green:
-						if ( gs.GreenProps.Count() >= gs.MaxProps )
-						{
-							hud?.FlashPropsFailed();
-							return;
-						}
-						break;
-					case Team.Yellow:
-						if ( gs.YellowProps.Count() >= gs.MaxProps )
-						{
-							hud?.FlashPropsFailed();
-							return;
-						}
-						break;
-				}
-
-				GameObject.Dispatch( new WeaponAnimEvent( "b_attack", true ) );
-
-				var gb = new GameObject();
-
-				var renderer = gb.Components.Create<Prop>();
-				var fortWarsProp = gb.Components.Create<FortwarsProp>();
-
-				renderer.Model = Prop;
-				fortWarsProp.Health = renderer.Health;
-
-				if ( fortWarsProp.Health == 0 )
-					fortWarsProp.Invincible = true;
-
-				// Break the prop into individuals so we can check for ModelPhysics later.
-				renderer.Break();
-
-				gb.WorldPosition = ObjectPos;
-				gb.WorldRotation = PropRotation.SnapToGrid( 15 );
-
-				var team = player.TeamComponent;
-
-				if ( team.IsValid() )
-					fortWarsProp.Team = team.Team;
-
-				fortWarsProp.CanKill = false;
-
-				if ( PropIdent is not null && (gs.ClassicIndents?.TryGetValue( PropIdent, out var propHealth ) ?? false) && propHealth > 0 )
-					renderer.Health = propHealth;
-
-				if ( gb.Components.TryGet<Rigidbody>( out var rb, FindMode.EverythingInSelfAndParent ) )
-				{
-					fortWarsProp.Rigidbody = rb;
-
-					rb.PhysicsBody.BodyType = PhysicsBodyType.Static;
-				}
-
-
-				gb.Network.SetOwnerTransfer( OwnerTransfer.Takeover );
-				gb.NetworkSpawn();
-
-				// We need to do this for props with ModelPhysics since they start with MotionEnabled on, 
-				// which breaks the positioning for the local client.
-				if ( gb.Components.TryGet<ModelPhysics>( out var mdlPhys, FindMode.EnabledInSelf ) )
-				{
-					mdlPhys.MotionEnabled = false;
-					Invoke( 0.1f, () => mdlPhys.MotionEnabled = true );
-				}
-
-				if ( rb.IsValid() )
-					SetStaticBodyType( rb );
+				PlaceProp();
 			}
 		}
 	}
@@ -252,5 +124,227 @@ public sealed class Propgun : Item
 			foreach ( var panel in hud.Panel.ChildrenOfType<SpawnerMenu>() )
 				panel.Delete();
 		}
+
+		Mouse.Visible = false;
+		UsingMouseInput = false;
+	}
+
+	public void PlacePropMouse()
+	{
+		var camera = Scene.Camera;
+
+		if ( !camera.IsValid() )
+			return;
+
+		var tr = Scene.Trace
+			.Box( Prop.Bounds.Rotate( PropRotation ) * 0.75f, Scene.Camera.ScreenPixelToRay( Mouse.Position ), 200f )
+			.IgnoreGameObjectHierarchy( GameObject.Root )
+			.Run();
+
+		bool CanPlace = tr.Hit && tr.Distance > 32.0f && tr.EndPosition.Distance( GameObject.Root.WorldPosition ) > 32.0f && !tr.GameObject.Tags.Has( FW.Tags.NoBuild );
+
+		var pos = tr.EndPosition.SnapToGrid( 16, true, true, !(tr.Hit && tr.Normal == Vector3.Up) );
+
+		var gizmo = Gizmo.Draw.Model( Prop.ResourcePath );
+		gizmo.ColorTint = Color.White.WithAlpha( 0.5f );
+		gizmo.Rotation = PropRotation.SnapToGrid( 15 );
+		gizmo.Position = pos;
+
+		if ( !CanPlace )
+		{
+			gizmo.ColorTint = Color.Red.WithAlpha( 0.5f );
+		}
+
+		if ( tr.Hit && Input.Pressed( "destroy" ) && (tr.GameObject?.Root?.Components.TryGet<FortwarsProp>( out var prop, FindMode.EverythingInSelfAndDescendants ) ?? false) )
+		{
+			if ( prop.Invincible )
+				return;
+
+			tr.GameObject?.Root?.Destroy();
+		}
+
+		if ( Input.Pressed( "reload" ) )
+		{
+			PropRotation = Rotation.Identity;
+		}
+
+		if ( CanPlace && Input.Pressed( "attack1" ) )
+		{
+			var sound = Sound.Play( "sounds/toolgun/toolgun-fire.sound", WorldPosition );
+			sound.Volume = 0.5f;
+
+			if ( !PlayerController.Local.TeamComponent.IsValid() )
+				return;
+
+			SpawnProp( pos );
+		}
+	}
+
+	public void PlaceProp()
+	{
+		var player = PlayerController.Local;
+
+		if ( !player.IsValid() )
+			return;
+
+		Vector3 ObjectPos = player.Eye.WorldPosition + player.Eye.WorldRotation.Forward * 100.0f;
+
+		var tr = Scene.Trace
+			.Box( Prop.Bounds.Rotate( PropRotation ) * 0.75f, player.Eye.WorldPosition, player.Eye.WorldPosition + player.Eye.WorldRotation.Forward * 200.0f )
+			.IgnoreGameObjectHierarchy( GameObject.Root )
+			.Run();
+
+		if ( tr.Hit )
+		{
+			ObjectPos = tr.EndPosition;
+		}
+
+		ObjectPos = ObjectPos.SnapToGrid( 16, true, true, !(tr.Hit && tr.Normal == Vector3.Up) );
+
+		bool CanPlace = tr.Hit && tr.Distance > 32.0f && ObjectPos.Distance( GameObject.Root.WorldPosition ) > 32.0f && !tr.GameObject.Tags.Has( FW.Tags.NoBuild );
+
+		var gizmo = Gizmo.Draw.Model( Prop.ResourcePath );
+		gizmo.ColorTint = Color.White.WithAlpha( 0.5f );
+		gizmo.Rotation = PropRotation.SnapToGrid( 15 );
+		gizmo.Position = ObjectPos;
+
+
+		if ( !CanPlace )
+		{
+			gizmo.ColorTint = Color.Red.WithAlpha( 0.5f );
+		}
+
+		player.CanMoveHead = !Input.Down( "attack2" );
+
+		if ( tr.Hit && Input.Pressed( "destroy" ) && (tr.GameObject?.Root?.Components.TryGet<FortwarsProp>( out var prop, FindMode.EverythingInSelfAndDescendants ) ?? false) )
+		{
+			if ( prop.Invincible )
+				return;
+
+			tr.GameObject?.Root?.Destroy();
+		}
+
+		if ( Input.Down( "attack2" ) )
+		{
+			var rot = Input.AnalogLook.WithRoll( 0 );
+
+			PropRotation += new Angles( -rot.pitch, -rot.yaw, 0 );
+		}
+
+		if ( Input.Pressed( "reload" ) )
+		{
+			PropRotation = Rotation.Identity;
+		}
+
+
+		if ( CanPlace && Input.Pressed( "attack1" ) )
+		{
+			var sound = Sound.Play( "sounds/toolgun/toolgun-fire.sound", WorldPosition );
+			sound.Volume = 0.5f;
+
+			if ( !player.TeamComponent.IsValid() )
+				return;
+
+			SpawnProp( ObjectPos );
+		}
+	}
+
+	public void SpawnProp( Vector3 ObjectPos )
+	{
+		var player = PlayerController.Local;
+
+		if ( !player.IsValid() )
+			return;
+
+		var team = player.TeamComponent;
+
+		var gs = GameSystem.Instance;
+
+		if ( !gs.IsValid() || !team.IsValid() )
+			return;
+
+		var currentTeam = team.Team;
+
+		var hud = Scene.GetAll<HUD>()?.FirstOrDefault();
+
+		switch ( currentTeam )
+		{
+			case Team.Red:
+				if ( gs.RedProps.Count() >= gs.MaxProps )
+				{
+					hud?.FlashPropsFailed();
+					return;
+				}
+				break;
+			case Team.Blue:
+				if ( gs.BlueProps.Count() >= gs.MaxProps )
+				{
+					hud?.FlashPropsFailed();
+					return;
+				}
+				break;
+			case Team.Green:
+				if ( gs.GreenProps.Count() >= gs.MaxProps )
+				{
+					hud?.FlashPropsFailed();
+					return;
+				}
+				break;
+			case Team.Yellow:
+				if ( gs.YellowProps.Count() >= gs.MaxProps )
+				{
+					hud?.FlashPropsFailed();
+					return;
+				}
+				break;
+		}
+
+		GameObject.Dispatch( new WeaponAnimEvent( "b_attack", true ) );
+
+		var gb = new GameObject();
+
+		var renderer = gb.Components.Create<Prop>();
+		var fortWarsProp = gb.Components.Create<FortwarsProp>();
+
+		renderer.Model = Prop;
+		fortWarsProp.Health = renderer.Health;
+
+		if ( fortWarsProp.Health == 0 )
+			fortWarsProp.Invincible = true;
+
+		// Break the prop into individuals so we can check for ModelPhysics later.
+		renderer.Break();
+
+		gb.WorldPosition = ObjectPos;
+		gb.WorldRotation = PropRotation.SnapToGrid( 15 );
+
+		if ( team.IsValid() )
+			fortWarsProp.Team = team.Team;
+
+		fortWarsProp.CanKill = false;
+
+		if ( PropIdent is not null && (gs.ClassicIndents?.TryGetValue( PropIdent, out var propHealth ) ?? false) && propHealth > 0 )
+			renderer.Health = propHealth;
+
+		if ( gb.Components.TryGet<Rigidbody>( out var rb, FindMode.EverythingInSelfAndParent ) )
+		{
+			fortWarsProp.Rigidbody = rb;
+
+			rb.PhysicsBody.BodyType = PhysicsBodyType.Static;
+		}
+
+		gb.Network.SetOwnerTransfer( OwnerTransfer.Takeover );
+		gb.NetworkSpawn();
+
+		// We need to do this for props with ModelPhysics since they start with MotionEnabled on, 
+		// which breaks the positioning for the local client.
+		if ( gb.Components.TryGet<ModelPhysics>( out var mdlPhys, FindMode.EnabledInSelf ) )
+		{
+			mdlPhys.MotionEnabled = false;
+			Invoke( 0.1f, () => mdlPhys.MotionEnabled = true );
+		}
+
+		if ( rb.IsValid() )
+			SetStaticBodyType( rb );
 	}
 }
