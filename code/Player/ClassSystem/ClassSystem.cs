b65a023282f2a27302132f06ca82a6a286e3604c
Nolankicks <nolandimensions@gmail.com>
2024-10-27 11:25:37 -0400

    Remove ClassIcon from ClassSystem.cs (we don't use it at all). Write description for sniper. Delete terrorist.class

--- code/Player/ClassSystem/ClassSystem.cs
+++ code/Player/ClassSystem/ClassSystem.cs
@@ -7,7 +7,6 @@ public sealed class PlayerClass : GameResource
 	public WeaponData WeaponData { get; set; }
 	public string ClassName { get; set; }
 	public string ClassDescription { get; set; }
-	public Texture ClassIcon { get; set; }
 	public int WalkSpeed { get; set; } = 300;
 	public int RunSpeed { get; set; } = 450;
 	public int Health { get; set; } = 100;
