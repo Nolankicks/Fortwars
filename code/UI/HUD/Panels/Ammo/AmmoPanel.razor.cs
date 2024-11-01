39e0449e2cbb1153ae837eeff17b7cd21d7d2b57
ScoutWozniak <99102549+ScoutWozniak@users.noreply.github.com>
2024-11-01 23:28:07 +1000

    Ammo counter

---
+++ code/UI/HUD/Panels/Ammo/AmmoPanel.razor.cs
@@ -0,0 +1,28 @@
+﻿using Sandbox.UI;
+using System;
+public partial class AmmoPanel : Panel
+{
+	Item weapon;
+
+	public override void Tick()
+	{
+		var inv = FWPlayerController.Local.Inventory;
+		if ( inv.CurrentItem.IsValid() )
+		{
+			weapon = inv.CurrentItem.Components.Get<Item>();
+		}
+	}
+
+	protected override int BuildHash()
+	{
+		var hash = new HashCode();
+		var inv = FWPlayerController.Local.Inventory;
+		hash.Add( weapon );
+		if ( inv.CurrentItem.Components.TryGet<Item>( out var item ) )
+		{
+			hash.Add( item.Ammo );
+			hash.Add( item.UsesAmmo );
+		}
+		return hash.ToHashCode();
+	}
+}
