c6966324a2bc05b7c222e9c43f343fa567f73c72
ScoutWozniak <99102549+ScoutWozniak@users.noreply.github.com>
2024-11-05 17:00:00 +1000

    Dynamic crosshair

--- code/UI/HUD/Panels/Crosshair/Crosshair.razor.cs
+++ code/UI/HUD/Panels/Crosshair/Crosshair.razor.cs
@@ -9,12 +9,23 @@ public partial class Crosshair : Panel
 
 	private RenderAttributes attributes { get; set; }
 
+	private float stringLength { get; set; } = 16.0f;
+	private float stringGap { get; set; } = 8.0f;
+
+	private float borderWidth { get; set; } = 1.0f;
+	private float borderRadius { get; set; } = 2.0f;
+
+	public Color ColorTint { get; set; } = Color.White;
+
+	private FWPlayerController local { get; set; }
+
 	protected override void OnAfterTreeRender( bool firstTime )
 	{
 		base.OnAfterTreeRender( firstTime );
 		attributes = new RenderAttributes();
-		attributes.Set( "Texture", Texture.Load( "textures/Crosshairs/crosshair008.vtex" ) );
+		attributes.Set( "Texture", Texture.White );
 
+		local = FWPlayerController.Local;
 	}
 
 	protected override int BuildHash()
@@ -28,9 +39,44 @@ public partial class Crosshair : Panel
 	{
 		base.DrawBackground( ref state );
 
-		var pos = new Vector2( 0, 0 );
-		var center = Screen.Size / 2;
+		if ( !local.IsValid() )
+			return;
 
-		Graphics.DrawQuad( Box.RectOuter, Material.UI.Basic, Color.White, attributes );
+		float playerSpeed = local.shrimpleCharacterController.Velocity.Length * 0.05f;
+
+		if ( !local.shrimpleCharacterController.IsOnGround )
+			playerSpeed += 5.0f;
+
+		if ( local.IsCrouching )
+			playerSpeed -= 2.0f;
+
+
+		var centerRect = Box.RectOuter;
+		DrawSegment( centerRect );
+
+		var rightRect = Box.RectOuter;
+		rightRect.Left += centerRect.Width + stringGap + playerSpeed;
+		rightRect.Width = stringLength;
+		DrawSegment( rightRect );
+
+		var leftRect = Box.RectOuter;
+		leftRect.Left -= stringLength + stringGap + playerSpeed;
+		leftRect.Width = stringLength;
+		DrawSegment( leftRect );
+
+		var topRect = Box.RectOuter;
+		topRect.Top -= stringLength + stringGap + playerSpeed;
+		topRect.Height = stringLength;
+		DrawSegment( topRect );
+
+		var bottomRect = Box.RectOuter;
+		bottomRect.Top += centerRect.Height + stringGap + playerSpeed;
+		bottomRect.Height = stringLength;
+		DrawSegment( bottomRect );
+	}
+
+	void DrawSegment( Rect rect )
+	{
+		Graphics.DrawRoundedRectangle( rect, ColorTint, new Vector4( borderRadius ), new Vector4( borderWidth ), Color.Black );
 	}
 }
