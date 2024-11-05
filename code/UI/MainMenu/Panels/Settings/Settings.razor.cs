31f6c393c019f644de4bea297e34d85b73b77bdb
ScoutWozniak <99102549+ScoutWozniak@users.noreply.github.com>
2024-11-05 20:46:20 +1000

    New settings menu (hidden)

---
+++ code/UI/MainMenu/Panels/Settings/Settings.razor.cs
@@ -0,0 +1,10 @@
+﻿using Sandbox.UI;
+
+public partial class Settings : Panel
+{
+	void Close()
+	{
+		var parent = Scene.GetAllComponents<MainMenu>().FirstOrDefault();
+		parent.ChangeMenuState( MainMenu.MenuState.None );
+	}
+}
