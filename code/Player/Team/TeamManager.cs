d66a98826848e44eebcf23af8763f820bcd89ee8
ScoutWozniak <99102549+ScoutWozniak@users.noreply.github.com>
2024-11-06 21:00:01 +1000

    Setup prop inventory

--- code/Player/Team/TeamManager.cs
+++ code/Player/Team/TeamManager.cs
@@ -5,8 +5,21 @@
 
 	public static int MaxBudget { get; set; } = 1000;
 
+	[Sync] public NetDictionary<PropResource, int> PropCounts { get; set; } = new NetDictionary<PropResource, int>();
+
 	public static TeamManager GetManager( Team team )
 	{
 		return Game.ActiveScene.GetAllComponents<TeamManager>().Where( x => x.Team == team ).FirstOrDefault();
+	}
+
+	protected override void OnEnabled()
+	{
+		if ( IsProxy )
+			return;
+
+		foreach ( var prop in ResourceLibrary.GetAll<PropResource>().Where( x => !x.Hidden ) )
+		{
+			PropCounts.Append( new( prop, 10 ) );
+		}
 	}
 }
