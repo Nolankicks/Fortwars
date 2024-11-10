29258b808684f943eb2dff7d671a49874f66de75
ScoutWozniak <99102549+ScoutWozniak@users.noreply.github.com>
2024-11-10 21:42:07 +1000

    Use action graph to check round change

--- code/Gamemodes/Base/RoundComponent.cs
+++ code/Gamemodes/Base/RoundComponent.cs
@@ -2,9 +2,12 @@ using System;
 
 public sealed class RoundComponent : Component
 {
+	[Header( "Metadata" )]
 	[Property] public string Name { get; set; }
 
-	enum EndConditions
+
+
+	enum EndTypes
 	{
 		[Title( "Timer" ), Icon( "Timer" ),
 			Description( "End the round when the timer runs out" )]
@@ -13,17 +16,20 @@ public sealed class RoundComponent : Component
 		Description( "End the round when a team reaches a certain score (DONE VIA ACTION)" )]
 		E_CONDITION,
 	}
-	[Property] EndConditions EndCondition { get; set; }
+	[Header( "End Conditions" )]
+	[Property] EndTypes EndType { get; set; }
+
+	[Property, ShowIf( "EndType", EndTypes.E_CONDITION )] public Func<bool> EndCondition { get; set; }
+
+	[Header( "Inventory" )]
 
 	[Property] public bool UseClasses { get; set; }
 
 	[Property, ShowIf( "UseClasses", false )] List<WeaponData> PlayerWeapons { get; set; }
 
+
+	[Header( "Actions" )]
 	[Property, Category( "Actions" )] public Action OnRoundStart { get; set; }
 	[Property, Category( "Actions" )] public Action OnRoundEnd { get; set; }
 
-	protected override void OnUpdate()
-	{
-
-	}
 }
