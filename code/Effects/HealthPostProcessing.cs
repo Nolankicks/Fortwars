3ffe50c53199c66dbc94e082fcb95bc490a1455b
ScoutWozniak <99102549+ScoutWozniak@users.noreply.github.com>
2024-11-16 22:15:38 +1000

    Initial damage vignette

---
+++ code/Effects/HealthPostProcessing.cs
@@ -0,0 +1,28 @@
+public sealed class HealthPostProcessing : Component
+{
+	[Property] HealthComponent Health { get; set; }
+
+	private Vignette vignette { get; set; }
+
+	protected override void OnStart()
+	{
+		vignette = Scene.GetAllComponents<Vignette>().FirstOrDefault();
+	}
+
+	protected override void OnUpdate()
+	{
+		vignette.Color = vignette.Color.WithAlpha( vignette.Color.a.LerpTo( GetIntensity(), Time.Delta * 10.0f ) );
+	}
+
+	float GetIntensity()
+	{
+		var percent = 1.0f - ((float)Health.Health / (float)Health.MaxHealth);
+
+		if ( percent < 0.5f )
+			return 0.0f;
+
+		percent -= 0.5f;
+
+		return percent * percent;
+	}
+}
