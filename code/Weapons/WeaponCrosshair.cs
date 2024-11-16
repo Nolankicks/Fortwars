9b5c81a9b642ce564f8dfb189b300259c2818195
ScoutWozniak <99102549+ScoutWozniak@users.noreply.github.com>
2024-11-16 23:27:28 +1000

    Dynamic ammo counter

--- code/Weapons/WeaponCrosshair.cs
+++ code/Weapons/WeaponCrosshair.cs
@@ -2,6 +2,7 @@ using Sandbox.Rendering;
 
 public sealed class WeaponCrosshair : Component
 {
+
 	[Property, Range( 1.0f, 8.0f, clamped: false )] float Size { get; set; } = 4.0f;
 	[Property] bool BorderEnabled { get; set; } = true;
 	[Property, HideIf( "BorderEnabled", false )] float BorderSize { get; set; } = 1.0f;
@@ -13,7 +14,9 @@ public sealed class WeaponCrosshair : Component
 	[Property] bool TopEnabled { get; set; } = true;
 
 	private float DynamicGap { get; set; } = 0.0f;
-	[Category( "Ammo" )] public Weapon Weapon { get; set; }
+	[Category( "Ammo" ), Property] public Weapon Weapon { get; set; }
+
+	[Category( "Ammo" ), Property] float AmmoAngle { get; set; } = 180.0f;
 
 	protected override void OnUpdate()
 	{
@@ -45,36 +48,38 @@ public sealed class WeaponCrosshair : Component
 		float _end = _gap + Length;
 
 
-		DrawLine( hud, center + left * _gap, center + left * _end );
-		DrawLine( hud, center - left * _gap, center - left * _end );
+		DrawLine( hud, center + left * _gap, center + left * _end, Color.White, Size );
+		DrawLine( hud, center - left * _gap, center - left * _end, Color.White, Size );
 
 
-		DrawLine( hud, center + up * _gap, center + up * _end );
+		DrawLine( hud, center + up * _gap, center + up * _end, Color.White, Size );
 		if ( TopEnabled )
-			DrawLine( hud, center - up * _gap, center - up * _end );
+			DrawLine( hud, center - up * _gap, center - up * _end, Color.White, Size );
 
 		if ( !Weapon.IsValid() )
 			return;
 
-		var bullets = Weapon.Ammo;
+		var bullets = Weapon.MaxAmmo;
 
 		for ( int i = 0; i < bullets; i++ )
 		{
-			float angle = i * (90 / bullets);
+			float angle = (i + 0.5f) * (AmmoAngle / bullets);
 			Vector2 dir = new Angles( 0, angle, 0 ).Forward;
-			Vector2 offset = Vector2.Up * 8.0f + Vector2.Left * 8.0f;
-			DrawLine( hud, center + dir * 32.0f + offset, center + dir * 40.0f + offset );
+			float dist = 38.0f;
+			DrawLine( hud, center + dir * dist, center + dir * (dist + 8.0f), i + 1 > Weapon.Ammo ? Color.Gray.WithAlpha( 0.5f ) : Color.White.WithAlpha( 0.5f ), 3.0f );
 		}
+		float counterDist = 48.0f;
+		hud.DrawText( new TextRendering.Scope( $"{Weapon.Ammo}/{Weapon.MaxAmmo}", Color.White, 16.0f, font: "IBMPlexMono", weight: 600 ), center + Vector2.Up * counterDist + Vector2.Left * counterDist );
 	}
 
 
 
-	void DrawLine( HudPainter hud, Vector2 start, Vector2 end )
+	void DrawLine( HudPainter hud, Vector2 start, Vector2 end, Color col, float size )
 	{
 		// Note the border is really buggy right now
 		if ( BorderEnabled )
-			hud.DrawLine( start, end, Size + BorderSize, Color.Black, corners: Vector4.One * 4.0f );
+			hud.DrawLine( start, end, size + BorderSize, Color.Black, corners: Vector4.One * 4.0f );
 
-		hud.DrawLine( start, end, Size, Color.White, corners: Vector4.One * 4.0f );
+		hud.DrawLine( start, end, size, col, corners: Vector4.One * 4.0f );
 	}
 }
