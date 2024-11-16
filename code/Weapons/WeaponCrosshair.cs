eab268580391e1e8a5bf691f0f074e703058eafd
ScoutWozniak <99102549+ScoutWozniak@users.noreply.github.com>
2024-11-16 23:00:30 +1000

    Update crosshairs

--- code/Weapons/WeaponCrosshair.cs
+++ code/Weapons/WeaponCrosshair.cs
@@ -13,6 +13,7 @@ public sealed class WeaponCrosshair : Component
 	[Property] bool TopEnabled { get; set; } = true;
 
 	private float DynamicGap { get; set; } = 0.0f;
+	[Category( "Ammo" )] public Weapon Weapon { get; set; }
 
 	protected override void OnUpdate()
 	{
@@ -51,10 +52,29 @@ public sealed class WeaponCrosshair : Component
 		DrawLine( hud, center + up * _gap, center + up * _end );
 		if ( TopEnabled )
 			DrawLine( hud, center - up * _gap, center - up * _end );
+
+		if ( !Weapon.IsValid() )
+			return;
+
+		var bullets = Weapon.Ammo;
+
+		for ( int i = 0; i < bullets; i++ )
+		{
+			float angle = i * (90 / bullets);
+			Vector2 dir = new Angles( 0, angle, 0 ).Forward;
+			Vector2 offset = Vector2.Up * 8.0f + Vector2.Left * 8.0f;
+			DrawLine( hud, center + dir * 32.0f + offset, center + dir * 40.0f + offset );
+		}
 	}
+
+
 
 	void DrawLine( HudPainter hud, Vector2 start, Vector2 end )
 	{
+		// Note the border is really buggy right now
+		if ( BorderEnabled )
+			hud.DrawLine( start, end, Size + BorderSize, Color.Black, corners: Vector4.One * 4.0f );
+
 		hud.DrawLine( start, end, Size, Color.White, corners: Vector4.One * 4.0f );
 	}
 }
