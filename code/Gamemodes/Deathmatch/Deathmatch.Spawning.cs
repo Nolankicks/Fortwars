3b2c7c212a5c015151fc8db37d3f041cdba62d1a
ScoutWozniak <99102549+ScoutWozniak@users.noreply.github.com>
2024-11-17 13:45:59 +1000

    Fix deathmatch spawning

--- code/Gamemodes/Deathmatch/Deathmatch.Spawning.cs
+++ code/Gamemodes/Deathmatch/Deathmatch.Spawning.cs
@@ -1,14 +1,26 @@
-using Sandbox;
 using Sandbox.Citizen;
+using Sandbox.Events;
 
 public partial class Deathmatch
 {
 	public override void OnActive( Connection connection )
 	{
+		connection.CanRefreshObjects = true;
+
 		if ( !GameSystem.PlayerPrefab.IsValid() || !GameSystem.SpawnPlayer )
 			return;
 
-		var player = GameSystem.PlayerPrefab.Clone();
+		var spawns = Scene.GetAllComponents<TeamSpawnPoint>().Where( x => x.Team == Team.None ).ToList();
+		Transform SpawnTransform = spawns.Count > 0 ? Game.Random.FromList( spawns ).Transform.World : Transform.World;
+
+		var player = GameSystem.PlayerPrefab.Clone( SpawnTransform );
+
+		if ( player.Components.TryGet<FWPlayerController>( out var p ) )
+		{
+			p.SetWorld( SpawnTransform );
+
+			p.TeleportToAnySpawnPoint();
+		}
 
 		if ( player.Components.TryGet<CitizenAnimationHelper>( out var animHelper, FindMode.EnabledInSelfAndChildren ) && animHelper.Target.IsValid() )
 		{
@@ -17,11 +29,25 @@ public partial class Deathmatch
 			clothing.Apply( animHelper.Target );
 		}
 
-		if ( player.Components.TryGet<FWPlayerController>( out var playerController ) )
+		player.NetworkSpawn( connection );
+
+		var mode = Scene.GetAll<GameMode>()?.FirstOrDefault();
+
+		if ( mode.IsValid() && mode.CurrentRound.IsValid() )
 		{
-			playerController.TeleportToAnySpawnPoint();
+			mode.CurrentRound.SetActiveRound();
+
+			mode.CurrentRound.OnPlayerJoin?.Invoke( player );
+
+			if ( mode.CurrentRound.PlayerWeapons.Any() && player.Components.TryGet<Inventory>( out var i ) )
+			{
+				i.AddItems( mode.CurrentRound.PlayerWeapons );
+			}
 		}
 
-		player.NetworkSpawn( connection );
+		//We need a better way of joining mid game. But I'm not sure what the best way is. Maybe an action on the round gameobject?
+
+		//Event callback
+		Scene.Dispatch( new OnPlayerJoin() );
 	}
 }
