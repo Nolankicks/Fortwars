57db117bdbae9f9c895ae0000d539ea547ee6cb1
Kicks <nolandimensions@gmail.com>
2024-11-19 09:27:00 -0500

    WIP interactive dedicated server console

--- code/MapSystem/MapGameObjectSystem.cs
+++ code/MapSystem/MapGameObjectSystem.cs
@@ -5,22 +5,118 @@ public sealed class MapLoadingSystem : GameObjectSystem<MapLoadingSystem>, IScen
 		Listen( Stage.SceneLoaded, 0, OnSceneLoad, "OnSceneLoad" );
 	}
 
-	void ISceneStartup.OnHostInitialize()
-	{
-		Log.Info( "Host Initialized" );
-
-		//If we are a dedicated server, load a scene
-		if ( Application.IsHeadless )
-			Scene.LoadFromFile( "scenes/easter.scene" );
-	}
-
 	void OnSceneLoad()
 	{
 		//Don't load the engine scene if it's already loaded
-		if ( Scene.GetAll<GameSystem>().Count() > 0 || Scene.IsEditor || Scene.GetAll<MainMenu>().Count() > 0 )
+		if ( Scene.GetAll<GameSystem>().Any() || Scene.IsEditor || Scene.GetAll<MainMenu>().Any() || Scene.GetAll<SceneLoaderBlocker>().Any() )
 			return;
 
 		var core = GameObject.Clone( ResourceLibrary.Get<PrefabFile>( "prefabs/core.prefab" ) );
 		core.BreakFromPrefab();
 	}
+
+	void ISceneStartup.OnHostInitialize()
+	{
+		if ( !Application.IsHeadless )
+			return;
+
+		Log.Info( "Welcome to the Fortwars Dedicated Server!" );
+		Log.Info( "" );
+		Log.Info( "To load a map, use the 'loadmap' command followed by the map name listed below." );
+		Log.Info( "" );
+		Log.Info( "Available maps:" );
+
+		foreach ( var map in ResourceLibrary.GetAll<MapInfo>().Where( x => !x.Hidden ) )
+		{
+			Log.Info( $"- {map.MapName}" );
+		}
+
+		Log.Info( "" );
+		Log.Info( "To set the map use gamemode use the 'loadgamemode' command followed by the gamemode name." );
+		Log.Info( "" );
+		Log.Info( "Available gamemodes:" );
+
+		foreach ( var gamemode in ResourceLibrary.GetAll<GameModeResource>().Where( x => !x.Hidden ) )
+		{
+			Log.Info( $"- {gamemode.ResourceName}" );
+		}
+
+		Log.Info( "" );
+		Log.Info( "To start the game, use the 'startgame' command." );
+	}
+
+	public static MapInfo CurrentMap { get; private set; }
+	public static GameModeResource CurrentGamemode { get; private set; }
+
+	[ConCmd( "loadmap" )]
+	public static void LoadMap( string mapName )
+	{
+		if ( !Application.IsHeadless )
+		{
+			Log.Warning( "This command can only be used on a dedicated server!" );
+			return;
+		}
+
+		var map = ResourceLibrary.GetAll<MapInfo>().FirstOrDefault( x => x.MapName == mapName );
+
+		if ( map is null )
+		{
+			Log.Warning( "Map not found!" );
+			return;
+		}
+
+		Log.Info( $"Map set to {map.MapName}" );
+
+		CurrentMap = map;
+	}
+
+	[ConCmd( "loadgamemode" )]
+	public static void LoadGamemode( string gamemodeName )
+	{
+		if ( !Application.IsHeadless )
+		{
+			Log.Warning( "This command can only be used on a dedicated server!" );
+			return;
+		}
+
+		var gamemode = ResourceLibrary.GetAll<GameModeResource>().FirstOrDefault( x => x.ResourceName == gamemodeName );
+
+		if ( gamemode is null )
+		{
+			Log.Warning( "Gamemode not found!" );
+			return;
+		}
+
+		Log.Info( $"Gamemode set to {gamemode.ResourceName}" );
+
+		CurrentGamemode = gamemode;
+	}
+
+	[ConCmd( "startgame" )]
+	public static void StartGame()
+	{
+		if ( !Application.IsHeadless )
+		{
+			Log.Warning( "This command can only be used on a dedicated server!" );
+			//return;
+		}
+
+		if ( CurrentMap is null )
+		{
+			Log.Warning( "No map selected! Falling back to default map: fw_easter" );
+			CurrentMap = ResourceLibrary.GetAll<MapInfo>().FirstOrDefault( x => x.MapName == "fw_easter" );
+		}
+
+		if ( CurrentGamemode is null )
+		{
+			Log.Warning( "No gamemode selected! Falling back to default gamemode: classic" );
+			CurrentGamemode = ResourceLibrary.GetAll<GameModeResource>().FirstOrDefault( x => x.ResourceName == "classic" );
+		}
+
+		GameSystem.SavedGameMode = CurrentGamemode;
+
+		Game.ActiveScene.Load( CurrentMap.Scene );
+	}
 }
+
+public sealed class SceneLoaderBlocker : Component { }
