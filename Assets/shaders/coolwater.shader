736e20f13b4f736cb1787038b2cd3ea4a3c25910
Kicks <nolandimensions@gmail.com>
2024-11-27 00:04:31 -0500

    Water you doing?

---
+++ Assets/shaders/coolwater.shader
@@ -0,0 +1,100 @@
+HEADER
+{
+	Description = "";
+}
+
+FEATURES
+{
+	#include "common/features.hlsl"
+}
+
+MODES
+{
+	VrForward();
+	Depth(); 
+	ToolsVis( S_MODE_TOOLS_VIS );
+	ToolsWireframe( "vr_tools_wireframe.shader" );
+	ToolsShadingComplexity( "tools_shading_complexity.shader" );
+}
+
+COMMON
+{
+	#include "common/shared.hlsl"
+	#include "procedural.hlsl"
+
+	#define CUSTOM_MATERIAL_INPUTS
+}
+
+struct VertexInput
+{
+	#include "common/vertexinput.hlsl"
+};
+
+struct PixelInput
+{
+	#include "common/pixelinput.hlsl"
+	float4 vScreenSpacePosition : TEXCOORD13;
+};
+
+VS
+{
+	#include "common/vertex.hlsl"
+
+	float4 ScreenSpacePosition(float4 position)
+	{
+		float4 v = position * 0.5;
+		v.xy += v.w;
+		v.zw = position.zw;
+		return v;
+	}
+
+	PixelInput MainVs( VertexInput v )
+	{
+		PixelInput i = ProcessVertex( v );
+		PixelInput o = FinalizeVertex( i );
+		o.vScreenSpacePosition = ScreenSpacePosition( i.vPositionPs );
+		return o;
+	}
+}
+
+PS
+{
+	#include "common/pixel.hlsl"
+	
+	BoolAttribute( bWantsFBCopyTexture, true );
+	BoolAttribute( translucent, true );
+
+	SamplerState g_sSampler0 < Filter( ANISO ); AddressU( WRAP ); AddressV( WRAP ); >;
+
+	Texture2D g_tReflection < Attribute( "Reflection" ); SrgbRead( true ); >;
+	Texture2D g_tRefraction < Attribute( "FrameBufferCopyTexture" ); SrgbRead( true ); >;
+
+	CreateInputTexture2D( Normal, Srgb, 8, "None", "_normal", ",0/,0/0", Default4( 1.00, 1.00, 1.00, 1.00 ) );
+	Texture2D g_tNormal < Channel( RGBA, Box( Normal ), Srgb ); OutputFormat( DXT5 ); SrgbRead( false ); >;
+
+	CreateInputTexture2D( Fresnel, Srgb, 8, "None", "_color", ",0/,0/0", Default4( 1.00, 1.00, 1.00, 1.00 ) );
+	Texture2D g_tFresnel < Channel( RGBA, Box( Fresnel ), Srgb ); OutputFormat( DXT5 ); SrgbRead( false ); >;
+
+	float4 MainPs( PixelInput i ) : SV_Target0
+	{
+		float2 wave = i.vTextureCoords.xy * 1;
+		float3 normal1 = Tex2DS( g_tNormal, g_sSampler0, wave + (g_flTime * 0.025) ).xyz * 2.0 - 1.0;
+		float3 normal2 = Tex2DS( g_tNormal, g_sSampler0, wave + 0.5 + (g_flTime * 0.04) ).xyz * 2.0 - 1.0;
+		float3 normal = (normal1 + normal2) * 0.5;
+
+		float4 uv1 = i.vScreenSpacePosition;
+		uv1.xy += normal.xy * 15;
+		uv1.xy *= -1.0;
+
+		float2 uv2 = i.vPositionSs.xy * g_vFrameBufferCopyInvSizeAndUvScale.xy;
+		uv2.xy -= normal.xy * 0.5;
+
+		float3 reflectionColor = Tex2DS( g_tReflection, g_sSampler0, uv1.xy / uv1.w ).rgb;
+		float3 refractionColor = Tex2DS( g_tRefraction, g_sSampler0, uv2 ).rgb * float3( 0.5, 0.9, 1.0 );
+
+		float fresnelFactor = dot( normalize( -i.vPositionWithOffsetWs.xyz ), normal );
+		fresnelFactor = Tex2DS( g_tFresnel, g_sSampler0, float2( fresnelFactor, fresnelFactor ) ).r;
+
+		return float4( lerp( refractionColor, reflectionColor, fresnelFactor ), 1.0 );
+	}
+}
---
+++ code/Effects/ReflectivePlane.cs
@@ -0,0 +1,194 @@
+using System;
+
+using Matrix4x4 = System.Numerics.Matrix4x4;
+
+namespace SdfWorld;
+
+public sealed class ReflectivePlane : Component
+{
+	[RequireComponent] public ModelRenderer Renderer { get; private set; } = null!;
+
+	private CameraComponent? _reflectionCamera;
+	private Texture? _renderTexture;
+
+	protected override void OnEnabled()
+	{
+		if ( _reflectionCamera.IsValid() ) return;
+
+		var cameraObj = new GameObject( false, "Reflection Camera" );
+
+		_reflectionCamera = cameraObj.AddComponent<CameraComponent>();
+		_reflectionCamera.RenderExcludeTags.Add( "water" );
+		_reflectionCamera.IsMainCamera = false;
+	}
+
+	protected override void OnDisabled()
+	{
+		CleanUp();
+	}
+
+	protected override void OnDestroy()
+	{
+		CleanUp();
+	}
+
+	private void CleanUp()
+	{
+		_reflectionCamera?.DestroyGameObject();
+		_reflectionCamera = null;
+
+		_renderTexture?.Dispose();
+		_renderTexture = null;
+	}
+
+	protected override void OnUpdate()
+	{
+		if ( _reflectionCamera is { Active: true } )
+		{
+			// For some reason I can't set this during / before the first OnPreRender
+			_reflectionCamera.Priority = -100;
+		}
+	}
+
+	protected override void OnPreRender()
+	{
+		if ( Scene.Camera is not { } mainCamera ) return;
+
+		var targetSize = mainCamera.ScreenRect.Size;
+
+		if ( _renderTexture is null || !_renderTexture.Size.AlmostEqual( targetSize ) )
+		{
+			_renderTexture?.Dispose();
+			_renderTexture = Texture.CreateRenderTarget()
+				.WithScreenFormat()
+				.WithSize( mainCamera.ScreenRect.Size )
+				.Create( "Reflection" );
+			
+			_reflectionCamera!.RenderTarget = _renderTexture;
+			_reflectionCamera.GameObject.Enabled = true;
+
+			Renderer.SceneObject.Attributes.Set( "Reflection", _renderTexture );
+		}
+
+		var plane = new Plane( WorldPosition, WorldRotation.Up );
+		var cameraPosition = mainCamera.WorldPosition;
+		var cameraRotation = mainCamera.WorldRotation;
+
+		var viewMatrix = Matrix.CreateWorld( cameraPosition, cameraRotation.Forward, cameraRotation.Up );
+		var reflectMatrix = ReflectMatrix( viewMatrix, plane );
+
+		var reflectionPosition = reflectMatrix.Transform( cameraPosition );
+		var reflectionRotation = ReflectRotation( cameraRotation, plane.Normal );
+
+		_reflectionCamera!.WorldPosition = reflectionPosition;
+		_reflectionCamera.WorldRotation = reflectionRotation;
+		_reflectionCamera.BackgroundColor = mainCamera.BackgroundColor;
+		_reflectionCamera.ZNear = mainCamera.ZNear;
+		_reflectionCamera.ZFar = mainCamera.ZFar;
+		_reflectionCamera.FieldOfView = mainCamera.FieldOfView;
+
+		var projectionMatrix = CreateProjection( _reflectionCamera );
+		var cameraSpaceClipNormal = _reflectionCamera.WorldRotation.Inverse * WorldRotation.Up;
+
+		// Swizzle so +x is right, +z is forward etc
+		cameraSpaceClipNormal = new Vector3(
+			cameraSpaceClipNormal.y,
+			-cameraSpaceClipNormal.z,
+			cameraSpaceClipNormal.x ).Normal;
+
+		projectionMatrix = ModifyProjectionMatrix( projectionMatrix,
+			new Vector4( cameraSpaceClipNormal, Vector3.Dot( reflectionPosition - WorldPosition, WorldRotation.Up ) ) );
+
+		_reflectionCamera.CustomProjectionMatrix = projectionMatrix;
+
+		WorldPosition = mainCamera.WorldPosition.SnapToGrid( 256f ).WithZ( WorldPosition.z );
+	}
+
+	private static Matrix ReflectMatrix( Matrix4x4 m, Plane plane )
+	{
+		m.M11 = (1.0f - 2.0f * plane.Normal.x * plane.Normal.x);
+		m.M21 = (-2.0f * plane.Normal.x * plane.Normal.y);
+		m.M31 = (-2.0f * plane.Normal.x * plane.Normal.z);
+		m.M41 = (-2.0f * -plane.Distance * plane.Normal.x);
+
+		m.M12 = (-2.0f * plane.Normal.y * plane.Normal.x);
+		m.M22 = (1.0f - 2.0f * plane.Normal.y * plane.Normal.y);
+		m.M32 = (-2.0f * plane.Normal.y * plane.Normal.z);
+		m.M42 = (-2.0f * -plane.Distance * plane.Normal.y);
+
+		m.M13 = (-2.0f * plane.Normal.z * plane.Normal.x);
+		m.M23 = (-2.0f * plane.Normal.z * plane.Normal.y);
+		m.M33 = (1.0f - 2.0f * plane.Normal.z * plane.Normal.z);
+		m.M43 = (-2.0f * -plane.Distance * plane.Normal.z);
+
+		m.M14 = 0.0f;
+		m.M24 = 0.0f;
+		m.M34 = 0.0f;
+		m.M44 = 1.0f;
+
+		return m;
+	}
+
+	private static Matrix CreateProjection( CameraComponent camera )
+	{
+		var tanAngleHorz = MathF.Tan( camera.FieldOfView * 0.5f * MathF.PI / 180f );
+		var tanAngleVert = tanAngleHorz * camera.ScreenRect.Height / camera.ScreenRect.Width;
+
+		return CreateProjection( tanAngleHorz, tanAngleVert, camera.ZNear, camera.ZFar );
+	}
+	private static float Dot( Vector4 a, Vector4 b )
+	{
+		return System.Numerics.Vector4.Dot( a, b );
+	}
+
+	private static Matrix CreateProjection( float tanAngleHorz, float tanAngleVert, float nearZ, float farZ )
+	{
+		var invReverseDepth = 1f / (nearZ - farZ);
+
+		var result = new Matrix4x4(
+			1f / tanAngleHorz, 0f, 0f, 0f,
+			0f, 1f / tanAngleVert, 0f, 0f,
+			0f, 0f, farZ * invReverseDepth, farZ * nearZ * invReverseDepth,
+			0f, 0f, -1f, 0f
+		);
+
+		return result;
+	}
+
+	/// <summary>
+	/// Pinched from <see href="https://terathon.com/blog/oblique-clipping.html">here</see>
+	/// and <see href="https://forum.beyond3d.com/threads/oblique-near-plane-clipping-reversed-depth-buffer.52827/">here</see>.
+	/// </summary>
+	private static Matrix ModifyProjectionMatrix( Matrix matrix, Vector4 clipPlane )
+	{
+		Matrix4x4 m = matrix;
+
+		// Calculate the clip-space corner point opposite the clipping plane
+		// as (sgn(clipPlane.x), sgn(clipPlane.y), 1, 1) and
+		// transform it into camera space by multiplying it
+		// by the inverse of the projection matrix
+
+		Vector4 q = default;
+
+		q.x = (MathF.Sign( clipPlane.x ) - m.M13) / m.M11;
+		q.y = (MathF.Sign( clipPlane.y ) - m.M23) / m.M22;
+		q.z = 1f;
+		q.w = (1f - m.M33) / m.M34;
+
+		// Calculate the scaled plane vector
+		var c = clipPlane * (1f / Dot( clipPlane, q ));
+
+		// Replace the third row of the projection matrix
+		m.M31 = -c.x;
+		m.M32 = -c.y;
+		m.M33 = -c.z;
+		m.M34 = c.w;
+
+		return m;
+	}
+
+	private static Rotation ReflectRotation( Rotation source, Vector3 normal )
+	{
+		return Rotation.LookAt( Vector3.Reflect( source * Vector3.Forward, normal ), Vector3.Reflect( source * Vector3.Up, normal ) );
+	}
+}
