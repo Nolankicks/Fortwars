15425080170b7c3556af17fdd67067e5ac79fac6
ScoutWozniak <99102549+ScoutWozniak@users.noreply.github.com>
2024-11-28 20:18:39 +1000

    Gizmo for nav markers

--- code/NavMarker.cs
+++ code/NavMarker.cs
@@ -8,4 +8,19 @@ public sealed class NavMarker : Component
 		base.OnUpdate();
 		Gizmo.Draw.LineSphere( WorldPosition, 4.0f );
 	}
+
+	protected override void DrawGizmos()
+	{
+
+
+		Gizmo.Draw.Color = Tint;
+		Gizmo.Transform = Scene.Transform.World;
+		Gizmo.Draw.Sprite( WorldPosition, 12.0f, "textures/ui/navmarker.png" );
+
+		// Lets only draw the text while close
+		if ( WorldPosition.Distance( Gizmo.Camera.Position ) > 250 )
+			return;
+		Gizmo.Draw.Text( Text, Transform.World );
+
+	}
 }
