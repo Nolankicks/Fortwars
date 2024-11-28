a49417a13e6e2519c5c1c68a5e5718a4871277ad
ScoutWozniak <99102549+ScoutWozniak@users.noreply.github.com>
2024-11-28 20:07:28 +1000

    Can tint marker

--- code/NavMarker.cs
+++ code/NavMarker.cs
@@ -1,9 +1,11 @@
-using Sandbox;
-
 public sealed class NavMarker : Component
 {
+	[Property] public Color Tint { get; set; } = Color.White.WithAlpha( 1.0f );
+	[Property] public string Text { get; set; }
+
 	protected override void OnUpdate()
 	{
-
+		base.OnUpdate();
+		Gizmo.Draw.LineSphere( WorldPosition, 4.0f );
 	}
 }
