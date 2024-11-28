e273abf6e3a21b7d3e5c17dba535546dec1ef25a
ScoutWozniak <99102549+ScoutWozniak@users.noreply.github.com>
2024-11-28 21:03:33 +1000

    Remove debug draw

--- code/NavMarker.cs
+++ code/NavMarker.cs
@@ -3,12 +3,6 @@ public sealed class NavMarker : Component
 	[Property] public Color Tint { get; set; } = Color.White.WithAlpha( 1.0f );
 	[Property] public string Text { get; set; }
 
-	protected override void OnUpdate()
-	{
-		base.OnUpdate();
-		Gizmo.Draw.LineSphere( WorldPosition, 4.0f );
-	}
-
 	protected override void DrawGizmos()
 	{
 		Gizmo.Draw.Color = Tint;
