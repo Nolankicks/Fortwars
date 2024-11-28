fb91b40df142193735febae07335821a4826addd
ScoutWozniak <99102549+ScoutWozniak@users.noreply.github.com>
2024-11-28 21:02:40 +1000

    Draw based on order

--- code/NavMarker.cs
+++ code/NavMarker.cs
@@ -11,8 +11,6 @@ public sealed class NavMarker : Component
 
 	protected override void DrawGizmos()
 	{
-
-
 		Gizmo.Draw.Color = Tint;
 		Gizmo.Transform = Scene.Transform.World;
 		Gizmo.Draw.Sprite( WorldPosition, 12.0f, "textures/ui/navmarker.png" );
@@ -21,6 +19,12 @@ public sealed class NavMarker : Component
 		if ( WorldPosition.Distance( Gizmo.Camera.Position ) > 250 )
 			return;
 		Gizmo.Draw.Text( Text, Transform.World );
+	}
 
+	public float GetDistance()
+	{
+		if ( !Scene.Camera.IsValid() )
+			return 0;
+		return Scene.Camera.WorldPosition.Distance( WorldPosition );
 	}
 }
