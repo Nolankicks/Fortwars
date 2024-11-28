1833b6bd9fe68c76f343baa2b5a0c5e8c2598763
ScoutWozniak <99102549+ScoutWozniak@users.noreply.github.com>
2024-11-28 14:16:27 +1000

    Dont respawn the player

--- code/Player/Weapons/Flag.cs
+++ code/Player/Weapons/Flag.cs
@@ -188,8 +188,6 @@ public sealed class CTFTrigger : Component, Component.ITriggerListener
 
 			local.Inventory.RemoveItem( flagComponent.GameObject );
 
-			local.TeamComponent?.ResetToSpawnPoint();
-
 			gs.AddFlagCapture( local.TeamComponent.Team );
 
 			PopupHolder.BroadcastPopup( $"{local.TeamComponent.Team} captured the flag!", 5 );
