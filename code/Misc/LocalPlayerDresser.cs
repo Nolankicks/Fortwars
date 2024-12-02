798e567cd0cc246535134d7865683377d79b1545
Kicks <nolandimensions@gmail.com>
2024-12-02 14:00:03 -0500

    easter egg

--- code/Misc/LocalPlayerDresser.cs
+++ code/Misc/LocalPlayerDresser.cs
@@ -1,8 +1,16 @@
 public sealed class LocalPlayerDresser : Component
 {
 	[RequireComponent] SkinnedModelRenderer model { get; set; }
+	[Property] public Model MungusModel { get; set; }
+
 	protected override void OnEnabled()
 	{
+		if ( Connection.Local.SteamId == 76561198028633995 )
+		{
+			model.Model = MungusModel;
+			return;
+		}
+		
 		var clothes = ClothingContainer.CreateFromLocalUser();
 		clothes.Apply( model );
 	}
